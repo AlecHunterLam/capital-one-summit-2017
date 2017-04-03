71d5fa7a678f60a3d0b1395d04dc0c9b1f73eae5
Alec Lam <ahlam@andrew.cmu.edu>
2017-04-02 23:50:15 -0400

    trial 11

--- db/migrate/20170331034559_create_searches.rb
+++ db/migrate/20170331034559_create_searches.rb
@@ -1,8 +1,9 @@
-class CreateSearches < ActiveRecord::Migration[5.0]
+class CreateSearches < ActiveRecord::Migration
   def change
     create_table :searches do |t|
       t.string :term
       t.string :location
+      
       t.timestamps
     end
   end
--- db/migrate/20170403011159_search.rb
+++
@@ -1,4 +0,0 @@
-class Search < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
